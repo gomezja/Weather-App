8f7fc22e3a2c7969d9f5f8b509194848394182a4
Jesus Gomez <gomezja@Jesuss-MacBook-Pro.local>
2017-07-05 16:14:10 -0700

    ui designed

---
+++ WeatherApp/WeatherVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Jesus Gomez on 7/5/17.
+//  Copyright © 2017 gomezja. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
